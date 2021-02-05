80422cf24631ceb075c637dea22360b057ab2938
Kyle Van Saders <kylev@affinityfcu.com>
2021-02-05 10:24:33 -0500

    initial commit

---
+++ SalesforcePocoGenerator/Program.cs
@@ -0,0 +1,184 @@
+﻿using Fclp;
+using Newtonsoft.Json;
+using Salesforce.Force;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace SalesforcePocoGenerator
+{
+    class Program
+    {
+        public class ApplicationArguments
+        {
+            public string ConsumerKey { get; set; }
+            public string ConsumerSecret { get; set; }
+            public string UserName { get; set; }
+            public string PasswordAndToken { get; set; }
+            public string TokenUrl { get; set; }
+            public string ObjectApiName { get; set; }
+
+            public string Namespace { get; set; }
+            public string FileOutput { get; set; }
+        }
+        static ApplicationArguments SetupCommandLineParser(string[] args)
+        {
+            var p = new FluentCommandLineParser<ApplicationArguments>();
+
+            p.Setup(arg => arg.ConsumerKey)
+             .As("ConsumerKey") 
+             .Required();
+
+            p.Setup(arg => arg.ConsumerSecret)
+             .As("ConsumerSecret")
+             .Required();
+
+            p.Setup(arg => arg.UserName)
+             .As("UserName")
+             .Required();
+
+            p.Setup(arg => arg.PasswordAndToken)
+             .As("PasswordAndToken")
+             .Required();
+
+            p.Setup(arg => arg.TokenUrl)
+              .As("TokenUrl")
+              .Required();
+
+            p.Setup(arg => arg.ObjectApiName)
+              .As("ObjectApiName")
+              .Required();
+
+            p.Setup(arg => arg.Namespace)
+              .As("Namespace")
+              .Required();
+
+            p.Setup(arg => arg.FileOutput)
+              .As("FileOutput");
+
+            var result = p.Parse(args);
+            if(result.HasErrors)
+            {
+                throw new Exception(result.ErrorText);
+            }
+
+            return p.Object;
+        }
+
+        static async Task Main(string[] args)
+        {
+            var appArguments = SetupCommandLineParser(args);
+            Console.WriteLine("Salesforce Object to Poco Generator for Rest API calls.");
+
+            ForceClient forceClient = null;
+            using (var auth = new Salesforce.Common.AuthenticationClient())
+            {
+                await auth.UsernamePasswordAsync(appArguments.ConsumerKey, appArguments.ConsumerSecret, appArguments.UserName, appArguments.PasswordAndToken, appArguments.TokenUrl);
+
+                forceClient = new ForceClient(auth.InstanceUrl, auth.AccessToken, "v50.0");
+            }
+            // get object description from Salesforce API
+            var describeObjectInfo = await forceClient.DescribeAsync<SalesforceObjectDescribe>(appArguments.ObjectApiName);
+
+            // generate c# class into string
+            var classStr = ($@"
+            namespace {appArguments.Namespace}
+            {{
+                public class {appArguments.ObjectApiName}
+                {{
+                    {string.Join("\r\n", describeObjectInfo.Fields.Select(x=>
+                      $" public {GetCSharpType(x.SoapType, x.Nillable)} {x.Name} {{get; set;}} "                                  
+                    ))
+                    }
+                }}
+            }}
+            ");
+
+            Console.WriteLine(classStr);
+
+            // write to file
+            if(! string.IsNullOrEmpty(appArguments.FileOutput))
+            {
+                File.WriteAllText(appArguments.FileOutput, "");
+            }
+
+        }
+
+        private static string GetCSharpType(string soapType, bool nillable)
+        {
+            switch(soapType)
+            {
+                case "tns:ID":
+                case "xsd:ID":
+                case "xsd:IDREF":
+                case "xsd:IDREFS":
+                case "xsd:language":
+                case "xsd:Name":
+                case "xsd:NMTOKEN":
+                case "xsd:NMTOKENS":
+                case "xsd:normalizedString":
+                case "xsd:QName":
+                case "xsd:token":
+                case "xsd:string":
+                case "xsd:anyURI":
+                case "xsd:NOTATION":
+                    return "string";
+                    break;
+                case "xsd:dateTime":
+                case "xsd:date":
+                    return nillable ? "DateTime?" : "DateTime";
+                    break;
+                case "xsd:decimal":
+                    return nillable ? "decimal?" : "decimal";
+                    break;
+                case "xsd:byte":
+                case "xsd:unsignedByte":
+                    return nillable ? "byte?" : "byte";
+                    break;
+                case "xsd:int":
+                case "xsd:negativeInteger":
+                case "xsd:nonNegativeInteger":
+                case "xsd:nonPositiveInteger":
+                case "xsd:positiveInteger":
+                    return nillable ? "int?" : "int";
+                    break;
+                case "xsd:short":
+                    return nillable ? "short?" : "short";
+                    break;
+                case "xsd:unsignedLong":
+                    return nillable ? "ulong?" : "ulong";
+                    break;
+                case "xsd:unsignedInt":
+                    return nillable ? "uint?" : "uint";
+                    break;
+                case "xsd:unsignedShort":
+                    return nillable ? "ushort?" : "ushort";
+                    break;
+
+                case "xsd:double":
+                    return nillable ? "double?" : "double";
+                    break;
+                case "xsd:float":
+                    return nillable ? "float?" : "float";
+                    break;
+
+
+
+                case "xsd:boolean":
+                    return nillable ? "bool?" : "bool";
+                    break;
+
+                case "xsd:base64Binary":
+                case "xsd:hexBinary":
+                    return "byte[]";
+                    break;
+
+            }
+
+            return "object";
+            
+        }
+    }
+}
---
+++ SalesforcePocoGenerator/SalesforceObjectDescribe.cs
@@ -0,0 +1,455 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesforcePocoGenerator
+{
+
+    public partial class SalesforceObjectDescribe
+    {
+        [JsonProperty("actionOverrides")]
+        public List<object> ActionOverrides { get; set; }
+
+        [JsonProperty("activateable")]
+        public bool Activateable { get; set; }
+
+        [JsonProperty("associateEntityType")]
+        public object AssociateEntityType { get; set; }
+
+        [JsonProperty("associateParentEntity")]
+        public object AssociateParentEntity { get; set; }
+
+        [JsonProperty("childRelationships")]
+        public List<ChildRelationship> ChildRelationships { get; set; }
+
+        [JsonProperty("compactLayoutable")]
+        public bool CompactLayoutable { get; set; }
+
+        [JsonProperty("createable")]
+        public bool Createable { get; set; }
+
+        [JsonProperty("custom")]
+        public bool Custom { get; set; }
+
+        [JsonProperty("customSetting")]
+        public bool CustomSetting { get; set; }
+
+        [JsonProperty("deepCloneable")]
+        public bool DeepCloneable { get; set; }
+
+        [JsonProperty("defaultImplementation")]
+        public object DefaultImplementation { get; set; }
+
+        [JsonProperty("deletable")]
+        public bool Deletable { get; set; }
+
+        [JsonProperty("deprecatedAndHidden")]
+        public bool DeprecatedAndHidden { get; set; }
+
+        [JsonProperty("extendedBy")]
+        public object ExtendedBy { get; set; }
+
+        [JsonProperty("extendsInterfaces")]
+        public object ExtendsInterfaces { get; set; }
+
+        [JsonProperty("feedEnabled")]
+        public bool FeedEnabled { get; set; }
+
+        [JsonProperty("fields")]
+        public List<Field> Fields { get; set; }
+
+        [JsonProperty("hasSubtypes")]
+        public bool HasSubtypes { get; set; }
+
+        [JsonProperty("implementedBy")]
+        public object ImplementedBy { get; set; }
+
+        [JsonProperty("implementsInterfaces")]
+        public object ImplementsInterfaces { get; set; }
+
+        [JsonProperty("isInterface")]
+        public bool IsInterface { get; set; }
+
+        [JsonProperty("isSubtype")]
+        public bool IsSubtype { get; set; }
+
+        [JsonProperty("keyPrefix")]
+        public string KeyPrefix { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("labelPlural")]
+        public string LabelPlural { get; set; }
+
+        [JsonProperty("layoutable")]
+        public bool Layoutable { get; set; }
+
+        [JsonProperty("listviewable")]
+        public object Listviewable { get; set; }
+
+        [JsonProperty("lookupLayoutable")]
+        public object LookupLayoutable { get; set; }
+
+        [JsonProperty("mergeable")]
+        public bool Mergeable { get; set; }
+
+        [JsonProperty("mruEnabled")]
+        public bool MruEnabled { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("namedLayoutInfos")]
+        public List<object> NamedLayoutInfos { get; set; }
+
+        [JsonProperty("networkScopeFieldName")]
+        public object NetworkScopeFieldName { get; set; }
+
+        [JsonProperty("queryable")]
+        public bool Queryable { get; set; }
+
+        [JsonProperty("recordTypeInfos")]
+        public List<RecordTypeInfo> RecordTypeInfos { get; set; }
+
+        [JsonProperty("replicateable")]
+        public bool Replicateable { get; set; }
+
+        [JsonProperty("retrieveable")]
+        public bool Retrieveable { get; set; }
+
+        [JsonProperty("searchLayoutable")]
+        public bool SearchLayoutable { get; set; }
+
+        [JsonProperty("searchable")]
+        public bool Searchable { get; set; }
+
+        [JsonProperty("sobjectDescribeOption")]
+        public string SobjectDescribeOption { get; set; }
+
+        [JsonProperty("supportedScopes")]
+        public List<SupportedScope> SupportedScopes { get; set; }
+
+        [JsonProperty("triggerable")]
+        public bool Triggerable { get; set; }
+
+        [JsonProperty("undeletable")]
+        public bool Undeletable { get; set; }
+
+        [JsonProperty("updateable")]
+        public bool Updateable { get; set; }
+
+        [JsonProperty("urls")]
+        public SalesforceObjectDescribeUrls Urls { get; set; }
+    }
+
+    public partial class ChildRelationship
+    {
+        [JsonProperty("cascadeDelete")]
+        public bool CascadeDelete { get; set; }
+
+        [JsonProperty("childSObject")]
+        public string ChildSObject { get; set; }
+
+        [JsonProperty("deprecatedAndHidden")]
+        public bool DeprecatedAndHidden { get; set; }
+
+        [JsonProperty("field")]
+        public string Field { get; set; }
+
+        [JsonProperty("junctionIdListNames")]
+        public List<object> JunctionIdListNames { get; set; }
+
+        [JsonProperty("junctionReferenceTo")]
+        public List<object> JunctionReferenceTo { get; set; }
+
+        [JsonProperty("relationshipName")]
+        public string RelationshipName { get; set; }
+
+        [JsonProperty("restrictedDelete")]
+        public bool RestrictedDelete { get; set; }
+    }
+
+    public partial class Field
+    {
+        [JsonProperty("aggregatable")]
+        public bool Aggregatable { get; set; }
+
+        [JsonProperty("aiPredictionField")]
+        public bool AiPredictionField { get; set; }
+
+        [JsonProperty("autoNumber")]
+        public bool AutoNumber { get; set; }
+
+        [JsonProperty("byteLength")]
+        public long ByteLength { get; set; }
+
+        [JsonProperty("calculated")]
+        public bool Calculated { get; set; }
+
+        [JsonProperty("calculatedFormula")]
+        public string CalculatedFormula { get; set; }
+
+        [JsonProperty("cascadeDelete")]
+        public bool CascadeDelete { get; set; }
+
+        [JsonProperty("caseSensitive")]
+        public bool CaseSensitive { get; set; }
+
+        [JsonProperty("compoundFieldName")]
+        public object CompoundFieldName { get; set; }
+
+        [JsonProperty("controllerName")]
+        public string ControllerName { get; set; }
+
+        [JsonProperty("createable")]
+        public bool Createable { get; set; }
+
+        [JsonProperty("custom")]
+        public bool Custom { get; set; }
+
+        [JsonProperty("defaultValue")]
+        public bool? DefaultValue { get; set; }
+
+        [JsonProperty("defaultValueFormula")]
+        public long? DefaultValueFormula { get; set; }
+
+        [JsonProperty("defaultedOnCreate")]
+        public bool DefaultedOnCreate { get; set; }
+
+        [JsonProperty("dependentPicklist")]
+        public bool DependentPicklist { get; set; }
+
+        [JsonProperty("deprecatedAndHidden")]
+        public bool DeprecatedAndHidden { get; set; }
+
+        [JsonProperty("digits")]
+        public long Digits { get; set; }
+
+        [JsonProperty("displayLocationInDecimal")]
+        public bool DisplayLocationInDecimal { get; set; }
+
+        [JsonProperty("encrypted")]
+        public bool Encrypted { get; set; }
+
+        [JsonProperty("externalId")]
+        public bool ExternalId { get; set; }
+
+        [JsonProperty("extraTypeInfo")]
+        public string ExtraTypeInfo { get; set; }
+
+        [JsonProperty("filterable")]
+        public bool Filterable { get; set; }
+
+        [JsonProperty("filteredLookupInfo")]
+        public FilteredLookupInfo FilteredLookupInfo { get; set; }
+
+        [JsonProperty("formulaTreatNullNumberAsZero")]
+        public bool FormulaTreatNullNumberAsZero { get; set; }
+
+        [JsonProperty("groupable")]
+        public bool Groupable { get; set; }
+
+        [JsonProperty("highScaleNumber")]
+        public bool HighScaleNumber { get; set; }
+
+        [JsonProperty("htmlFormatted")]
+        public bool HtmlFormatted { get; set; }
+
+        [JsonProperty("idLookup")]
+        public bool IdLookup { get; set; }
+
+        [JsonProperty("inlineHelpText")]
+        public object InlineHelpText { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("length")]
+        public long Length { get; set; }
+
+        [JsonProperty("mask")]
+        public object Mask { get; set; }
+
+        [JsonProperty("maskType")]
+        public object MaskType { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("nameField")]
+        public bool NameField { get; set; }
+
+        [JsonProperty("namePointing")]
+        public bool NamePointing { get; set; }
+
+        [JsonProperty("nillable")]
+        public bool Nillable { get; set; }
+
+        [JsonProperty("permissionable")]
+        public bool Permissionable { get; set; }
+
+        [JsonProperty("picklistValues")]
+        public List<PicklistValue> PicklistValues { get; set; }
+
+        [JsonProperty("polymorphicForeignKey")]
+        public bool PolymorphicForeignKey { get; set; }
+
+        [JsonProperty("precision")]
+        public long Precision { get; set; }
+
+        [JsonProperty("queryByDistance")]
+        public bool QueryByDistance { get; set; }
+
+        [JsonProperty("referenceTargetField")]
+        public object ReferenceTargetField { get; set; }
+
+        [JsonProperty("referenceTo")]
+        public List<string> ReferenceTo { get; set; }
+
+        [JsonProperty("relationshipName")]
+        public string RelationshipName { get; set; }
+
+        [JsonProperty("relationshipOrder")]
+        public object RelationshipOrder { get; set; }
+
+        [JsonProperty("restrictedDelete")]
+        public bool RestrictedDelete { get; set; }
+
+        [JsonProperty("restrictedPicklist")]
+        public bool RestrictedPicklist { get; set; }
+
+        [JsonProperty("scale")]
+        public long Scale { get; set; }
+
+        [JsonProperty("searchPrefilterable")]
+        public bool SearchPrefilterable { get; set; }
+
+        [JsonProperty("soapType")]
+        public string SoapType { get; set; }
+
+        [JsonProperty("sortable")]
+        public bool Sortable { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("unique")]
+        public bool Unique { get; set; }
+
+        [JsonProperty("updateable")]
+        public bool Updateable { get; set; }
+
+        [JsonProperty("writeRequiresMasterRead")]
+        public bool WriteRequiresMasterRead { get; set; }
+    }
+
+    public partial class FilteredLookupInfo
+    {
+        [JsonProperty("controllingFields")]
+        public List<object> ControllingFields { get; set; }
+
+        [JsonProperty("dependent")]
+        public bool Dependent { get; set; }
+
+        [JsonProperty("optionalFilter")]
+        public bool OptionalFilter { get; set; }
+    }
+
+    public partial class PicklistValue
+    {
+        [JsonProperty("active")]
+        public bool Active { get; set; }
+
+        [JsonProperty("defaultValue")]
+        public bool DefaultValue { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("validFor")]
+        public string ValidFor { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+
+    public partial class RecordTypeInfo
+    {
+        [JsonProperty("active")]
+        public bool Active { get; set; }
+
+        [JsonProperty("available")]
+        public bool Available { get; set; }
+
+        [JsonProperty("defaultRecordTypeMapping")]
+        public bool DefaultRecordTypeMapping { get; set; }
+
+        [JsonProperty("developerName")]
+        public string DeveloperName { get; set; }
+
+        [JsonProperty("master")]
+        public bool Master { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("recordTypeId")]
+        public string RecordTypeId { get; set; }
+
+        [JsonProperty("urls")]
+        public RecordTypeInfoUrls Urls { get; set; }
+    }
+
+    public partial class RecordTypeInfoUrls
+    {
+        [JsonProperty("layout")]
+        public string Layout { get; set; }
+    }
+
+    public partial class SupportedScope
+    {
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public partial class SalesforceObjectDescribeUrls
+    {
+        [JsonProperty("compactLayouts")]
+        public string CompactLayouts { get; set; }
+
+        [JsonProperty("rowTemplate")]
+        public string RowTemplate { get; set; }
+
+        [JsonProperty("approvalLayouts")]
+        public string ApprovalLayouts { get; set; }
+
+        [JsonProperty("uiDetailTemplate")]
+        public string UiDetailTemplate { get; set; }
+
+        [JsonProperty("uiEditTemplate")]
+        public string UiEditTemplate { get; set; }
+
+        [JsonProperty("defaultValues")]
+        public string DefaultValues { get; set; }
+
+        [JsonProperty("describe")]
+        public string Describe { get; set; }
+
+        [JsonProperty("uiNewRecord")]
+        public Uri UiNewRecord { get; set; }
+
+        [JsonProperty("quickActions")]
+        public string QuickActions { get; set; }
+
+        [JsonProperty("layouts")]
+        public string Layouts { get; set; }
+
+        [JsonProperty("sobject")]
+        public string Sobject { get; set; }
+    }
+}
